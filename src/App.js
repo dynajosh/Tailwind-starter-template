bfca3f85ed6fe4e5e1ac04eba4c464d741b64e51
josh <oriakhi78@gmail.com>
2021-10-26 08:20:44 +0100

    test page

--- src/App.js
+++ src/App.js
@@ -3,22 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+  <div className="flex-shrink-0">
+  </div>
+  <div>
+    <div className="text-xl font-medium text-black">Testing the app</div>
+    <p className="text-gray-500">Yeah, tailwind works!</p>
+  </div>
+</div>
   );
 }
 
